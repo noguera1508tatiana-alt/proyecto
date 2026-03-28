08f8eec8d9fdf316db12c12f798e212db242563a
noguera1508tatiana-alt <noguera1508tatiana@gmail.com>
2026-03-28 12:35:13 -0500

    primera fase Pr2

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-import streamlit as at 
+import streamlit as st 
 
-st.title("Mi Proyecto")
+st.title("Plataforma de Notas")
+st.subheader("BIENVENIDOS")
+st.title("Inicio de Sesión")
+st.text_input("Usuario")
+st.text_input("Contraseña")
+st.markdown("### Registrarse")
 
+if st.button("registrarse"):
+    pass
